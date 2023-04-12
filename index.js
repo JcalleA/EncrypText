815224766aecbef84d785319ae6225a43b775548
kalle-1413@hotmail.com <kalle-1413@hotmail.com>
2023-04-12 01:48:55 -0500

    ok

--- index.js
+++ index.js
@@ -2,12 +2,45 @@
 function encriptar(){
     document.getElementById('imagen1').style.display = "none";
     var texto = document.getElementById('entry').value;
-    var texto1=texto.replaceAll('a','ai')
-    var texto2=texto1.replaceAll('e','enter')
-    var texto3=texto2.replaceAll('i','imes')
-    var texto4=texto3.replaceAll('o','ober')
-    var texto5=texto4.replaceAll('u','ufat')
-    texto5.toLowerCase()
+    var texto1= ""
+    for (var i = 0; i < texto.length; i++) {
+      if(texto[i]=='a')
+        texto1+='ai'
+      else if(texto[i]=='e')
+        texto1+='enter'
+      else if(texto[i]=='i')
+        texto+='imes'
+      else if(texto[i]=='o')
+        texto1+='ober'
+      else if(texto[i]=='u')
+        texto1+='ufat'
+      else
+        texto1+=texto[i]
+  
+}
+    document.getElementById('parrafo').textContent=texto1
+    
+}
+
+function updateClipboard() {
+  var textCopy=document.getElementById('parrafo').innerText;
+  navigator.clipboard.writeText(textCopy).then(() => {
+    alert('copiado')
+    /* clipboard successfully set */
+  }, () => {
+    alert('error al copiar')
+    /* clipboard write failed */
+  });
+}
+
+function desencriptar(){
+    document.getElementById('imagen1').style.display = "none";
+    var texto = document.getElementById('entry').value;
+    var texto1=texto.replaceAll('ai','a')
+    var texto2=texto1.replaceAll('enter','e')
+    var texto3=texto2.replaceAll('imes','i')
+    var texto4=texto3.replaceAll('ober','o')
+    var texto5=texto4.replaceAll('ufat','u')
     document.getElementById('parrafo').textContent=texto5
     
 }
