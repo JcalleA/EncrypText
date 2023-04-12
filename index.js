907a1e831c2c3f3699c003b581874474d7a7bedf
kalle-1413@hotmail.com <kalle-1413@hotmail.com>
2023-04-12 00:13:56 -0500

    ok

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function encriptar(){
     document.getElementById('imagen1').style.display = "none";
     var texto = document.getElementById('entry').value;
-    var texto1=texto.replace('a','ai')
-    var texto2=texto1.replace('e','enter')
-    var texto3=texto2.replace('i','imes')
-    var texto4=texto3.replace('o','ober')
-    var texto5=texto4.replace('u','ufat')
-    console.log(texto5);
+    var texto1=texto.replaceAll('a','ai')
+    var texto2=texto1.replaceAll('e','enter')
+    var texto3=texto2.replaceAll('i','imes')
+    var texto4=texto3.replaceAll('o','ober')
+    var texto5=texto4.replaceAll('u','ufat')
+    texto5.toLowerCase()
     document.getElementById('parrafo').textContent=texto5
     
 }
